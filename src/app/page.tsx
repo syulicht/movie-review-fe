9bc5e451df4eb58f93812b35f5d4ae3de91016ab
yusato <visualankun@outlook.jp>
2024-12-28 20:18:28 +0900

    ratingStarの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/components/Header";
 
 const Home = (): React.JSX.Element => {
   return (
-    <div className="w-full flex flex-row justify-center">
+    <div className="w-full flex flex-col justify-center">
       <Header />
     </div>
   );
--- src/components/RatingStar.tsx
+++ src/components/RatingStar.tsx
@@ -1,24 +1,32 @@
 import React from "react";
-import { Rating } from "@smastrom/react-rating";
+import { Rating, Star } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
 
 type Props = {
   rate: number;
   readonly: boolean;
-  style: object;
 };
 
-const RatingStar = (props: Props): React.JSX.Element => {
+export const RatingStar = (props: Props): React.JSX.Element => {
   return (
-    <div>
-      <Rating
-        style={props.style}
-        items={5}
-        readOnly={props.readonly}
-        value={props.rate}
-      />
-    </div>
+    <Rating
+      itemStyles={
+        props.readonly
+          ? {
+              itemShapes: Star,
+              activeFillColor: "#daaa00",
+              inactiveFillColor: "#a2a2a2",
+            }
+          : {
+              itemShapes: Star,
+              activeFillColor: "#daaa00",
+              inactiveFillColor: "#a2a2a2",
+            }
+      }
+      style={props.readonly ? { width: "150px" } : { width: "300px" }}
+      items={5}
+      readOnly={props.readonly}
+      value={props.rate}
+    />
   );
 };
-
-export default RatingStar;
