a11472056f7d7a015f699207237d5541f14f4baa
yusato <visualankun@outlook.jp>
2024-12-28 14:15:16 +0900

    search fieldの修正

--- src/app/components/search.tsx
+++ src/app/components/search.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const search = () => {
   return (
-    <div className="w-2/5 bg-white h-12 rounded-2xl flex flex-column justify-between">
+    <div className="w-2/5 bg-white h-12 rounded-full flex flex-column justify-between items-center">
       <input
         type="text"
-        className="focus:outline-none h-12 ml-4 w-3/4"
+        className="focus:outline-none h-12 ml-4 w-3/4 h-3/4"
         placeholder="search..."
       />
       <button className="mr-4">
